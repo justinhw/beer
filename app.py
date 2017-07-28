9131f02540caa5930f8c122f31ef90543189f416
Justin Wong <justinwong12337@gmail.com>
2017-07-28 11:56:05 -0700

    Add config

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import os
 from flask import Flask
+
+
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
+    print("Using configs: {}".format(os.environ['APP_SETTINGS']))
     app.run()
